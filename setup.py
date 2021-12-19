9686256642fe463fde42e2af598de838512ac12b
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2021-12-19 16:15:07 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,15 @@ setup(
     download_url='https://github.com/lstnlp/HoogBERTa/archive/refs/tags/v0.1.0.tar.gz',
     keywords=['HoogBERTa: Multi-task Sequence Labeling using Thai Pretrained Language Representation'],
     classifiers=[
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: General',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
