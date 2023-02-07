e02e097219d99517812465cf5ece7bdf0cc61372
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2023-02-07 08:46:45 +0700

    update

--- hoogberta/syllable_encoder.py
+++ hoogberta/syllable_encoder.py
@@ -2,58 +2,62 @@ from fairseq.data.dictionary import Dictionary
 from fairseq.data.data_utils import collate_tokens
 from fairseq.models.roberta import RobertaModel
 
+from torch import nn
+
 from hoogberta.syllacut import tokenize
 
-class HoogBERTaSyllableEncoder(object):
+class HoogBERTaSyllableEncoder(nn.Module):
 
     def __init__(self,layer=12,cuda=False,base_path="."):
+        super().__init__()
+
         self.base_path = base_path
         roberta = RobertaModel.from_pretrained(self.base_path + '/models/', checkpoint_file='checkpoint.pt',data_name_or_path=self.base_path+"/models/")
         self.roberta = roberta
         self.roberta.eval()
+        self.dictionary = self.roberta.task.dictionary
         #print(self.roberta.model.encoder.sentence_encoder)
         #print(self.roberta.task.dictionary)
+
+        #print(type(self.roberta)) :: RobertaHubInterface
 
         if cuda == True:
             self.roberta = self.roberta.cuda()
 
-    
-    def extract_features(self,sentence):
+    def encode_line(self,line):
         all_sent = []
-        sentences = sentence.split(" ")
+        sentences = line.split(" ")
         for sent in sentences:
-            all_sent.append(tokenize(sent).replace("_","[!und:]"))
-        
-        sentence = "<s> " + " _ ".join(all_sent) #Add BOS
-        #print(sentence)
+            s = tokenize(sent)
+            #print(s)
+            all_sent.append(s.replace("_","[!und:]"))
 
-        encode = self.roberta.task.dictionary.encode_line(sentence)
+        #Add <s>, </s> will be automatically added
+        sentence = "<s> " + " _ ".join(all_sent) 
+        encode = self.dictionary.encode_line(sentence,add_if_not_exist=False)
+        return encode
+
+    def extract_features(self,sentence):
+        encode = self.encode_line(sentence)
         #print(encode)
-        
+        out = self.string(encode)
+        print(out)
         tokens = encode.unsqueeze(0)
-
         output = self.roberta.task.dictionary.string(encode)
         #print("DECODE",output)
         all_layers = self.roberta.extract_features(tokens, return_all_hiddens=True)
         return tokens[0], all_layers[-1][0]
 
-    def extract_features_batch(self,sentenceL):
-        
-        inputList = []
-        for sentX in sentenceL:
-            sentences = sentX.split(" ")
-            all_sent = []
-            for sent in sentences:
-                all_sent.append(tokenize(sent).replace("_","[!und:]"))
-            
-            sentence = " _ ".join(all_sent)
-            inputList.append(sentence)
+    def string(self,tokens):
+        return self.dictionary.string(tokens)
 
-        batch = collate_tokens([self.model.bert.encode(sent) for sent in inputList], pad_idx=1)
+
+    def extract_features_batch(self,sentenceL):
+        batch = collate_tokens([self.encode_line(sent) for sent in sentenceL], pad_idx=1)
         
         #tokens = self.model.bert.encode(inputList)
         return self.extract_features_from_tensor(batch)
 
     def extract_features_from_tensor(self,batch):
-        all_layers = self.model.bert.extract_features(batch, return_all_hiddens=True)
+        all_layers = self.roberta.extract_features(batch, return_all_hiddens=True)
         return batch, all_layers[-1]
--- test_encoder.py
+++ test_encoder.py
@@ -1,6 +1,11 @@
 from hoogberta.syllable_encoder import HoogBERTaSyllableEncoder
 
 model = HoogBERTaSyllableEncoder(base_path="../")
-output = model.extract_features("สวัสดีครับ")
+batch, output = model.extract_features("สวัสดีครับ123")
+print(output)
+
+print(model.string(batch))
+
+batch, output = model.extract_features_batch(["สวัสดีครับ","สวัสดีค่ะ"])
 print(output)
 
