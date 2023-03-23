bfddcb7fc767755e59ace10c525182b8e1876d00
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2023-03-23 10:37:35 +0700

    update dummy bpe

--- test_encoder.py
+++ test_encoder.py
@@ -9,3 +9,5 @@ print(model.string(batch))
 batch, output = model.extract_features_batch(["สวัสดีครับ","สวัสดีค่ะ"])
 print(output)
 
+#x = model.roberta.fill_mask('สวัสดีครับ123 <mask>', topk=3)
+#print(x)
