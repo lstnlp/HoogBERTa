fba77b7e117f7f35c7136a0e2bf1b81fe9ecdc05
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2023-03-23 10:46:53 +0700

    update

--- test_encoder.py
+++ test_encoder.py
@@ -1,6 +1,8 @@
 from hoogberta.syllable_encoder import HoogBERTaSyllableEncoder
+import os 
+model_home = os.environ.get('HoogBERTaSyllable')
 
-model = HoogBERTaSyllableEncoder(base_path="../")
+model = HoogBERTaSyllableEncoder(base_path=model_home)
 batch, output = model.extract_features("สวัสดีครับ123")
 print(output)
 
